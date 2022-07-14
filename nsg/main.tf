d1420ed4090763ff09ec65bae5a7a62fcdb01a3d
KushagraD146 <k.dubey146@gmail.com>
2022-07-14 17:53:51 +0530

    packer

---
+++ nsg/main.tf
@@ -0,0 +1,30 @@
+data "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+}
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = var.network_security_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = data.azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [
+    azurerm_network_security_group.app_nsg
+  ]
+}
