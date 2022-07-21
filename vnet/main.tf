4459ce3b5a00a3f8fd8bb7a6c7ad03a20686a0fd
Kushagra-Neu <106967877+Kushagra-Neu@users.noreply.github.com>
2022-07-21 15:57:22 +0530

    Delete vnet directory

--- vnet/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_virtual_network" "kd_vnet" {
-  name                = var.virtual_network_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = var.address_space_vnet
-  dns_servers         = var.dns_servers_vnet
-}
