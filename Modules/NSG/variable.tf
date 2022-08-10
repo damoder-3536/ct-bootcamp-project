a937ed1aa03575a5f6e539357e6a3972473b6ba2
Damodar Maranganti <Damodar.maranganti@neudesic.com>
2022-08-10 19:25:30 +0530

    added NSG rules via dynamic block

--- Modules/NSG/variable.tf
+++ Modules/NSG/variable.tf
@@ -26,6 +26,7 @@ variable "vnet_name" {
 }
 
 variable "nsg_rules" {
+  description = "The values for each NSG  rule"
   type = list(object({
     name  = string
     priority  = number
@@ -38,6 +39,42 @@ variable "nsg_rules" {
     description_address_prefix = string
 
   }))
+  default = [ {
+    access = "Allow"
+    description_address_prefix = "*"
+    destination_port_range = "*"
+    direction = "Inbound"
+    name = "Rule01"
+    priority = 100
+    protocol = "Tcp"
+    source_address_prefix = "*"
+    source_port_range = "80"
+  },
+  {
+    access = "Allow"
+    description_address_prefix = "*"
+    destination_port_range = "*"
+    direction = "Inbound"
+    name = "Rule02"
+    priority = 110
+    protocol = "Tcp"
+    source_address_prefix = "*"
+    source_port_range = "443"
+  },
+  {
+    access = "Allow"
+    description_address_prefix = "*"
+    destination_port_range = "*"
+    direction = "Inbound"
+    name = "Rule03"
+    priority = 120
+    protocol = "Tcp"
+    source_address_prefix = "*"
+    source_port_range = "3389"
+  }
+
+
+  ]
   
-  description = "The values for each NSG  rule"
+  
 }
